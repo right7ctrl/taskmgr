fcce999c2392cb0a84e109f0a5d4bb40b7c0748b
murat aslan <murataslanx33@gmail.com>
2021-01-15 22:13:00 +0300

    create list_item

--- lib/view/list/task_list.dart
+++ lib/view/list/task_list.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/widget/list/list_item.dart';
 
 class TaskList extends StatelessWidget {
   @override
@@ -6,6 +8,34 @@ class TaskList extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('List'),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(48),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: CupertinoTextField(
+              padding: const EdgeInsets.all(8),
+              placeholder: 'Search Task',
+              prefix: Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Icon(Icons.search),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.all(12),
+              itemBuilder: (context, index) {
+                return ListItem();
+              },
+              separatorBuilder: (context, index) => SizedBox(height: 12),
+              itemCount: 12,
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/widget/list/list_item.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 8,
+      color: Colors.white,
+      shadowColor: Colors.white70,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: Text(
+                  'Item title Item title Item title Item title Item title Item title',
+                  style: TextStyle(fontSize: 16),
+                )),
+                SizedBox(width: 24),
+                Container(
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.orange,
+                  ),
+                  child: Text(
+                    'Monthly',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+            Divider(height: 24),
+            Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(6),
+                        margin: EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                        child: Icon(Icons.delete),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(6),
+                        margin: EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                        child: Icon(Icons.delete),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(6),
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                        child: Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                ),
+                Chip(
+                  label: Text('Complete'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
